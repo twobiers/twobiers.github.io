e84464bd4bff8b16a1adbce2116b00b5371dd6d8
twobiers <22715034+twobiers@users.noreply.github.com>
2024-09-28 17:03:45 +0200

    Add script to create random post

---
+++ random.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -euo pipefail
+
+file="$(dirname $0)/content/random/$(date +%F).md"
+if [ ! -f "$file" ]; then
+    echo "* " > $file
+fi
+
+${VISUAL:-${EDITOR:-vi}} "${file}" || xdg-open "${file}"
